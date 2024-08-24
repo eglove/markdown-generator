7f2649c0552dc046c2a54b63d575027d2aca9ace
Ethan <hello@ethang.email>
2024-08-24 13:41:19 -0500

    init

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import config from "@ethang/eslint-config/eslint.config.js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(...config, {
+    languageOptions: {
+        parserOptions: {
+            project: true,
+            tsconfigRootDir: import.meta.dirname,
+        },
+    },
+});
---
+++ src/markdown-generator.ts
@@ -0,0 +1,88 @@
+export class MarkdownGenerator {
+  private markdown = "";
+
+  public alert(type: "CAUTION" | "IMPORTANT" | "NOTE" | "TIP" | "WARNING", text: string) {
+    this.markdown += `> [!${type}]\n`;
+    this.markdown += `> ${text}`;
+  }
+
+  public bold(text: string) {
+    this.markdown += `**${text}**`;
+  }
+
+  public codeBlock(text: string) {
+    this.markdown += `\`\`\`${text}\`\`\``;
+  }
+
+  public header(level: 1 | 2 | 3, text: string) {
+    let prefix = "";
+    for (let index = 1; index < level; index += 1) {
+      prefix += "#";
+    }
+
+    this.markdown += `${prefix} ${text}`;
+  }
+
+  public image(text: string, url: string) {
+    this.markdown = `![${text}](${url})`;
+  }
+
+  public inlineCode(text: string) {
+    this.markdown += `\`${text}\``;
+  }
+
+  public italic(text: string) {
+    this.markdown += `*${text}*`;
+  }
+
+  public link(text: string, url: string) {
+    this.markdown += `[${text}](${url})`;
+  }
+
+  public mention(text: string) {
+    this.markdown += `@${text}`;
+  }
+
+  public newLine() {
+    this.markdown += "\n";
+  }
+
+  public numberedList(texts: string[]) {
+    for (const text of texts) {
+      this.markdown += `1. ${text}\n`;
+    }
+  }
+
+  public quote(text: string) {
+    this.markdown += `> ${text}`;
+  }
+
+  public strickthrough(text: string) {
+    this.markdown += `~~${text}~~`;
+  }
+
+  public subscript(text: string) {
+    this.markdown += `<sub>${text}</sub>`;
+  }
+
+  public superscript(text: string) {
+    this.markdown += `<sup>${text}</sup>`;
+  }
+
+  public taskList(texts: {
+    isComplete: boolean;
+    label: string;
+  }[]) {
+    for (const text of texts) {
+      this.markdown += `[${text.isComplete
+        ? "X"
+        : " "}] ${text.label}`;
+    }
+  }
+
+  public unorderedList(texts: string[]) {
+    for (const text of texts) {
+      this.markdown += `* ${text}\n`;
+    }
+  }
+}
