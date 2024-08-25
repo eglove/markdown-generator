ac742a179b5474a8c350246d240f8cffac474ff8
Ethan <hello@ethang.email>
2024-08-25 10:06:58 -0500

    Add optional line breaks to MarkdownGenerator methods

--- src/markdown-generator.ts
+++ src/markdown-generator.ts
@@ -1,50 +1,60 @@
+import isArray from "lodash/isArray.js";
 import isNil from "lodash/isNil.js";
 
 export class MarkdownGenerator {
   private markdown = "";
 
-  public alert(type: "CAUTION" | "IMPORTANT" | "NOTE" | "TIP" | "WARNING", text: string) {
+  public alert(type: "CAUTION" | "IMPORTANT" | "NOTE" | "TIP" | "WARNING", text: string, lineBreaksAfter = 0) {
     this.markdown += `> [!${type}]\n`;
     this.markdown += `> ${text}`;
+    this.newLine(lineBreaksAfter);
   }
 
-  public bold(text: string) {
+  public bold(text: string, lineBreaksAfter = 0) {
     this.markdown += `**${text}**`;
+    this.newLine(lineBreaksAfter);
   }
 
-  public codeBlock(text: string, language = "") {
+  public codeBlock(text: string, language = "", lineBreaksAfter = 0) {
     this.markdown += `\`\`\`${language}
 ${text}
 \`\`\``;
+    this.newLine(lineBreaksAfter);
   }
 
-  public header(level: 1 | 2 | 3, text: string) {
+  public header(level: 1 | 2 | 3, text: string, lineBreaksAfter = 0) {
     let prefix = "";
     for (let index = 0; index < level; index += 1) {
       prefix += "#";
     }
 
     this.markdown += `${prefix} ${text}`;
+    this.newLine(lineBreaksAfter);
   }
 
-  public image(text: string, url: string) {
+  public image(text: string, url: string, lineBreaksAfter = 0) {
     this.markdown = `![${text}](${url})`;
+    this.newLine(lineBreaksAfter);
   }
 
-  public inlineCode(text: string) {
+  public inlineCode(text: string, lineBreaksAfter = 0) {
     this.markdown += `\`${text}\``;
+    this.newLine(lineBreaksAfter);
   }
 
-  public italic(text: string) {
+  public italic(text: string, lineBreaksAfter = 0) {
     this.markdown += `*${text}*`;
+    this.newLine(lineBreaksAfter);
   }
 
-  public link(text: string, url: string) {
+  public link(text: string, url: string, lineBreaksAfter = 0) {
     this.markdown += `[${text}](${url})`;
+    this.newLine(lineBreaksAfter);
   }
 
-  public mention(text: string) {
+  public mention(text: string, lineBreaksAfter = 0) {
     this.markdown += `@${text}`;
+    this.newLine(lineBreaksAfter);
   }
 
   public newLine(times?: number) {
@@ -57,60 +67,74 @@ ${text}
     }
   }
 
-  public numberedList(texts: string[], level = 0) {
+  public numberedList(texts: (string | string[])[], level = 0) {
     let space = "";
     for (let index = 0; index < level; index += 1) {
       space += " ";
     }
 
     for (const text of texts) {
-      this.markdown += `${space}1. ${text}\n`;
+      if (isArray(text)) {
+        this.numberedList(text, level + 1);
+      } else {
+        this.markdown += `${space}1. ${text}\n`;
+      }
     }
   }
 
-  public quote(text: string) {
+  public quote(text: string, lineBreaksAfter = 0) {
     this.markdown += `> ${text}`;
+    this.newLine(lineBreaksAfter);
   }
 
   public render() {
     return this.markdown;
   }
 
-  public strickthrough(text: string) {
+  public strickthrough(text: string, lineBreaksAfter = 0) {
     this.markdown += `~~${text}~~`;
+    this.newLine(lineBreaksAfter);
   }
 
-  public subscript(text: string) {
+  public subscript(text: string, lineBreaksAfter = 0) {
     this.markdown += `<sub>${text}</sub>`;
+    this.newLine(lineBreaksAfter);
   }
 
-  public superscript(text: string) {
+  public superscript(text: string, lineBreaksAfter = 0) {
     this.markdown += `<sup>${text}</sup>`;
+    this.newLine(lineBreaksAfter);
   }
 
   public taskList(texts: {
     isComplete: boolean;
     label: string;
-  }[]) {
+  }[], lineBreaksAfter = 0) {
     for (const text of texts) {
       this.markdown += `[${text.isComplete
         ? "X"
         : " "}] ${text.label}`;
     }
+    this.newLine(lineBreaksAfter);
   }
 
-  public text(text: string) {
+  public text(text: string, lineBreaksAfter = 0) {
     this.markdown += text;
+    this.newLine(lineBreaksAfter);
   }
 
-  public unorderedList(texts: string[], level = 0) {
+  public unorderedList(texts: (string | string[])[], level = 0) {
     let space = "";
     for (let index = 0; index < level; index += 1) {
       space += " ";
     }
 
     for (const text of texts) {
-      this.markdown += `${space}* ${text}\n`;
+      if (isArray(text)) {
+        this.unorderedList(text, level + 1);
+      } else {
+        this.markdown += `${space}* ${text}\n`;
+      }
     }
   }
 }
